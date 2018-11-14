2d4f772c31fa36ec6dfa8f5f007c0192d4414b81
Chris M <webmaster@magnaws.com>
2018-11-14 18:19:44 -0500

    Upped the threshold for min. brightness just a bit.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+**Version 1.0.0.2002** *(November 14, 2018)*
+- Fixed the way prefs are saved, so future releases shouldn't crash on startup.
+- Added an indicator if there is no music.
+- Made the darkener sliders more prominent when going dark.
+
 **Version 1.0.0.2000** *(November 13, 2018)*
 - First Beta Release
 
--- TheBestClock/View Controllers/MainScreenViewController.swift
+++ TheBestClock/View Controllers/MainScreenViewController.swift
@@ -96,7 +96,7 @@ class MainScreenViewController: UIViewController, UIPickerViewDelegate, UIPicker
                                              "TrebuchetMS-Bold",
                                              "Verdana-Bold"]
     /// This is our minimum brightness threshold. We don't let the text and stuff quite make it to 0.
-    let minimumBrightness: CGFloat = 0.05
+    let minimumBrightness: CGFloat = 0.15
     /// This is the base font size for the ante meridian label near the top of the screen.
     let amPmLabelFontSize: CGFloat = 30
     /// This is the base font size for the date display along the top.
