c73d1c6df87232afc5a21deb0f6f8578af5389be
Chris Marshall <chris@riftvalleysoftware.com>
2019-05-21 07:02:57 -0400

    Tweaked the jazzy file a bit.

--- jazzy.sh
+++ jazzy.sh
@@ -3,15 +3,16 @@ CWD="$(pwd)"
 MY_SCRIPT_PATH=`dirname "${BASH_SOURCE[0]}"`
 cd "${MY_SCRIPT_PATH}"
 rm -drf docs
-jazzy   --github_url https://github.com/RiftValleySoftware/amkamani\
---readme ./README.md \
---author The\ Great\ Rift\ Valley\ Software\ Company\
---author_url https://riftvalleysoftware.com\
---module AmkaMani \
---min-acl private \
---exclude=./*/*/TheGreatRiftValleyDrawing.swift,./*/TheBestClockSpeakerButton.swift \
--x CODE_SIGNING_ALLOWED=NO \
---copyright [©2019\ The\ Great\ Rift\ Valley\ Software\ Company]\(https://riftvalleysoftware.com\)
+jazzy   --github_url https://github.com/RiftValleySoftware/amkamani \
+        --readme ./README.md \
+        --theme fullwidth \
+        --author The\ Great\ Rift\ Valley\ Software\ Company \
+        --author_url https://riftvalleysoftware.com \
+        --module AmkaMani \
+        --min-acl private \
+        --exclude=./*/*/TheGreatRiftValleyDrawing.swift,./*/TheBestClockSpeakerButton.swift \
+        --copyright [©2019\ The\ Great\ Rift\ Valley\ Software\ Company]\(https://riftvalleysoftware.com\) \
+        -x CODE_SIGNING_ALLOWED=NO
 cp icon.png docs/icon.png
 cp img/*.* docs/img/
 cd "${CWD}"
